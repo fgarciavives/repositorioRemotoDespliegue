1cd13d0e409ee0266716754eb52414a7cfa20d99
Fran <99999873+fgarciavives@users.noreply.github.com>
2022-03-17 12:07:41 +0100

    ECLIPSE
    primer commit -2

---
+++ Fran.java
@@ -0,0 +1,20 @@
+package gitUno;
+
+public class Fran {
+    private String nombre;
+
+    public void set(String nombre) { this.nombre = nombre; }
+    public Object get() { return nombre; }
+    
+    public Fran(String x) {
+    	this.nombre = x;
+    }
+    
+    public static void main(String[] args) {
+    	System.out.print("hola");
+    }
+    
+    
+    
+}
+
